2d8364d5f38a1ec8d4075362f833871384487bff
[zitaoyu] <[zitaoyu.work@gmail.com]>
2022-01-18 04:25:41 -0800

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv/config');
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,8 +15,10 @@ const jokeRoute = require('./routes/jokes');
 
 app.use('/jokes', jokeRoute);
 
+//TO-DO
+const DB_CONNECTION = "your mongoDb url";
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+mongoose.connect(DB_CONNECTION, { useNewUrlParser: true }, () => {
     console.log('Connected to DB!!!');
 });
 
